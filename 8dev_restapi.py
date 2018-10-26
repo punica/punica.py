9a9dcd8b4791dc0c266f6c8cae8dfbfe733581a3
mrNedas <nedas@8devices.com>
2018-10-26 15:37:20 +0000

    Initial Service and Device classes commit

---
+++ 8dev_restapi.py
@@ -0,0 +1,31 @@
+import requests
+
+class Service:
+    host = 'http://localhost:8888'
+    ca = ''
+    authentication = False
+    username = ''
+    password = ''
+    interval = 1234
+    polling = False
+    port = 5728
+    
+    def start():
+        print('started')
+
+    def stop():
+    	print('started')
+
+    def get():
+        
+
+    def put(path, argument, contentType = 'application/vnd.oma.lwm2m+tlv'):
+        
+
+    def post(path, argument, contentType = 'application/vnd.oma.lwm2m+tlv'):
+        
+
+    def delete(path, argument, contentType = 'application/vnd.oma.lwm2m+tlv'):
+        
+    
+    
