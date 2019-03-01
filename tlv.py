842146816c2e06839a507927e761e8f01e7cce95
Nedas <nedas@8devices.com>
2019-03-01 12:42:24 +0200

    Added initial documentation

--- tlv.py
+++ tlv.py
@@ -1,3 +1,6 @@
+"""
+This module stores LwM2M TLV parsing methods
+"""
 from struct import pack, unpack
 import binascii
 import numbers
@@ -20,14 +23,40 @@ RESOURCE_TYPE = {
 
 
 def binary_to_integer(binary_data):
+    """Converts bytes to integer.
+
+    Parameters:
+    binaryData (bytearray): Buffer which will be converted.
+
+    Returns:
+    int: Integer value.
+    """
     return int(binascii.hexlify(binary_data), base=16)
 
 
 def binary_to_bit_string(binary_data):
+    """Converts bytes to string.
+
+    Parameters:
+    binaryData (bytearray): Buffer which will be converted.
+
+    Returns:
+    str: String value.
+    """
     return "{0:b}".format(binary_to_integer(binary_data))
 
 
 def get_dictionary_by_value(dictionary_list, key_name, value):
+    """Gets dictionary by given name of the key and value.
+
+    Parameters:
+    dictionary_list (object): Dictionary list.
+    key_name (object|str): Name of the key
+    value (object|str|int): Value
+
+    Returns:
+    object|str|int: dictionary
+    """
     for dictonary in dictionary_list:
         if dictonary[key_name] == value:
             return dictonary
@@ -35,6 +64,16 @@ def get_dictionary_by_value(dictionary_list, key_name, value):
 
 
 def change_buffer_size(buff, start, end=None):
+    """Changes buffer size.
+
+    Parameters:
+    buff (bytearray): Bytearray which size will be changed.
+    start (bytearray): Bytearray's start index.
+    start (bytearray): Bytearray's end index. (optional)
+
+    Returns:
+    bytearray: New size bytearray
+    """
     buffer_array = []
     index = start
     if not end:
@@ -60,6 +99,14 @@ def change_buffer_size(buff, start, end=None):
 
 
 def decode(buff):
+    """Decodes any TLV bytearray.
+
+    Parameters:
+    buff (bytearray): encoded TLV bytearray
+
+    Returns:
+    object: Decoded object
+    """
     i = None
     value_identifier = None
     index = 1
@@ -104,6 +151,15 @@ def decode(buff):
 
 
 def decode_resource_value(buff, resource):
+    """Decodes value of the resource.
+
+    Parameters:
+    buff (bytearray): Bytearray which will be decoded
+    resource (object): Object which stores resource value's type
+
+    Returns:
+    object|str|int|bool: Decoded value in specified type
+    """
     if resource['type'] == RESOURCE_TYPE['INTEGER']:
         if not buff:
             return 0
@@ -138,6 +194,15 @@ def decode_resource_value(buff, resource):
 
 
 def decode_multi_resource_instances(buff, resources):
+    """Decodes multiple resource TLV byte array.
+
+    Parameters:
+    buff (bytearray): TLV byte array
+    resources (object): Object which stores identifier and resource type
+
+    Returns:
+    object: Decoded resource.
+    """
     decoded_resource_values = []
     decoded_resource_instance = None
     index = 0
@@ -158,6 +223,15 @@ def decode_multi_resource_instances(buff, resources):
 
 
 def decode_resource(buff, resource):
+    """Decodes resource.
+
+    Parameters:
+    buff (bytearray): Resource TLV byte array
+    resources (object): Object which stores identifier and resource type
+
+    Returns:
+    object: Decoded resource.
+    """
     decoded_resource = decode(buff)
     resource_value = None
 
@@ -166,7 +240,8 @@ def decode_resource(buff, resource):
             'Decoded resource TLV identifier and description identifiers do not match')
 
     if decoded_resource['type'] == TYPE['RESOURCE']:
-        resource_value = decode_resource_value(decoded_resource['value'], resource)
+        resource_value = decode_resource_value(
+            decoded_resource['value'], resource)
     elif decoded_resource['type'] == TYPE['MULTIPLE_RESOURCE']:
         resource_value = decode_multi_resource_instances(
             decoded_resource['value'], resource)['value']
@@ -182,6 +257,14 @@ def decode_resource(buff, resource):
 
 
 def encode_resource_value(resource):
+    """Encodes value of the resource.
+
+    Parameters:
+    resource (object): Object which stores resource value and value's type
+
+    Returns:
+    bytearray: Byte array of encoded value
+    """
     min_int8 = -0x80
     max_int8 = 0x7f
     min_int16 = -0x8000
@@ -195,7 +278,9 @@ def encode_resource_value(resource):
         raise ValueError('Unrecognised type ', resource['type'])
 
     if resource['type'] == RESOURCE_TYPE['NONE']:
-        if not isinstance(resource['value'], int) or isinstance(resource['value'], float):
+        if not isinstance(
+                resource['value'],
+                int) or isinstance(resource['value'], float):
             raise ValueError('Unrecognised value type ',
                              type(resource['type']))
 
@@ -253,6 +338,15 @@ def encode_resource_value(resource):
 
 
 def encode(obj):
+    """Encodes ant type of instance (Object instance, multiple resources,
+    resources instance, resource).
+
+    Parameters:
+    obj (object): Object which stores type, identifier and value
+
+    Returns:
+    bytearray: Encoded TLV byte array
+    """
     identifier_buffer = None
     length_buffer = None
     type_byte = 0
@@ -297,10 +391,21 @@ def encode(obj):
 
         length_buffer = bytearray([])
 
-    return bytearray([type_byte]) + identifier_buffer + length_buffer + obj['value']
+    return bytearray([type_byte]) + identifier_buffer + \
+        length_buffer + obj['value']
 
 
 def encode_resource_instance(resource_instance):
+    """Encodes resource instance to TLV byte array.
+    resources instance, resource).
+
+    Parameters:
+    resource_instance (object): Object which stores resource identifier,
+    value and it's type.
+
+    Returns:
+    bytearray: Byte array in TLV format
+    """
     return encode({
         'type': TYPE['RESOURCE_INSTANCE'],
         'identifier': resource_instance['identifier'],
@@ -309,6 +414,15 @@ def encode_resource_instance(resource_instance):
 
 
 def encode_multiple_resources_tlv(resources):
+    """Encodes multiple resource values to TLV byte array.
+
+    Parameters:
+    resources (object): Object which stores identifier, resource type,
+    and multiple values
+
+    Returns:
+    bytearray: TLV byte array
+    """
     resource_instances_buffers = bytearray()
 
     for index in range(len(resources['value'])):
@@ -326,6 +440,14 @@ def encode_multiple_resources_tlv(resources):
 
 
 def encode_resource(resource):
+    """Encodes resource to TLV byte array.
+
+    Parameters:
+    resource (object): Object which stores resource identifier, type and value.
+
+    Returns:
+    bytearray: TLV byte array
+    """
     if isinstance(resource['value'], list):
         return encode_multiple_resources_tlv(resource)
 
@@ -337,10 +459,19 @@ def encode_resource(resource):
 
 
 def encode_object_instance(object_instance):
+    """Encodes LwM2M object instance to TLV byte array.
+
+    Parameters:
+    object_instance (object): LwM2M object instance
+
+    Returns:
+    bytearray: TLV byte array
+    """
     resources_buffers = bytearray()
 
     for index in range(len(object_instance['resources'])):
-        resources_buffers += encode_resource(object_instance['resources'][index])
+        resources_buffers += encode_resource(
+            object_instance['resources'][index])
 
     return encode({
         'type': TYPE['OBJECT_INSTANCE'],
@@ -350,6 +481,14 @@ def encode_object_instance(object_instance):
 
 
 def encode_object(obj):
+    """Encodes LwM2M object to TLV byte array
+
+    Parameters:
+    obj (object): LwM2M object
+
+    Returns:
+    bytearray: TLV byte array
+    """
     object_instances_buffers = bytearray()
 
     for index in range(len(obj['object_instances'])):
@@ -360,6 +499,16 @@ def encode_object(obj):
 
 
 def decode_resource_instance(buff, resources):
+    """Decodes resource instance.
+
+    Parameters:
+    buff (bytearray): Resource instance TLV byte array
+    resources (object): Object which stores resource identifier and resource type
+
+    Returns:
+    object: Object which stores resource identifier,
+    tlvSize resource type and value.
+    """
     decoded_resource_instance = decode(buff)
 
     if decoded_resource_instance['type'] != TYPE['RESOURCE_INSTANCE']:
@@ -368,24 +517,46 @@ def decode_resource_instance(buff, resources):
     return {
         'type': resources['type'],
         'identifier': decoded_resource_instance['identifier'],
-        'value': decode_resource_value(decoded_resource_instance['value'], resources),
+        'value': decode_resource_value(
+            decoded_resource_instance['value'],
+            resources),
         'tlvSize': decoded_resource_instance['tlvSize'],
     }
 
 
 def decode_resource_instance_value(buff, resource_instance):
+    """Decodes resource instance value
+
+    Parameters:
+    buff (bytearray): Resource instance TLV byte array
+    resource_instance (object): Object which stores resource type
+
+    Returns:
+    object: Decoded resource value
+    """
     decoded_resource_instance = decode(buff)
 
     if decoded_resource_instance['type'] != TYPE['RESOURCE_INSTANCE']:
         raise ValueError('Decoded resource TLV type is not resource instance')
 
     return {
-        'value': decode_resource_value(decoded_resource_instance['value'], resource_instance),
+        'value': decode_resource_value(
+            decoded_resource_instance['value'],
+            resource_instance),
         'tlvSize': decoded_resource_instance['tlvSize'],
     }
 
 
 def decode_object_instance(buff, object_instance):
+    """Decodes object instance from TLV byte array.
+
+    Parameters:
+    buff (bytearray): TLV byte array
+    object_instance (object): Object which stores object instance identifier and resources
+
+    Returns:
+    object:  Decoded object instance
+    """
     decoded_object_instance = decode(buff)
     decoded_resources = []
     remaining_buffer = None
@@ -396,17 +567,23 @@ def decode_object_instance(buff, object_instance):
 
     while index < len(decoded_object_instance['value']):
         remaining_buffer = change_buffer_size(
-            decoded_object_instance['value'], index, len(decoded_object_instance['value']))
+            decoded_object_instance['value'], index, len(
+                decoded_object_instance['value']))
         resource_identifier = decode(remaining_buffer)['identifier']
 
         resource_description = get_dictionary_by_value(
             object_instance['resources'], 'identifier', resource_identifier)
 
         if resource_description is None:
-            raise ValueError('No resource description found (x/',
-                             object_instance['identifier'], '/', resource_identifier, ')')
+            raise ValueError(
+                'No resource description found (x/',
+                object_instance['identifier'],
+                '/',
+                resource_identifier,
+                ')')
 
-        decoded_resource = decode_resource(remaining_buffer, resource_description)
+        decoded_resource = decode_resource(
+            remaining_buffer, resource_description)
         decoded_resources.append(decoded_resource)
         index += decoded_resource['tlvSize']
 
@@ -417,6 +594,15 @@ def decode_object_instance(buff, object_instance):
 
 
 def decode_object(buff, obj):
+    """Decodes LwM2M object to TLV byte array.
+
+    Parameters:
+    buff (bytearray): TLV byte array
+    obj (object): Object which stores object instances with their resources
+
+    Returns:
+    object:  Decoded object
+    """
     decoded_object_instances = []
     remaining_buffer = None
     object_instance_identifier = None
@@ -432,8 +618,12 @@ def decode_object(buff, obj):
             obj['object_instances'], 'identifier', object_instance_identifier)
 
         if object_instance_description is None:
-            raise ValueError('No object instance description found (',
-                             obj['identifier'], '/', object_instance_identifier, ')')
+            raise ValueError(
+                'No object instance description found (',
+                obj['identifier'],
+                '/',
+                object_instance_identifier,
+                ')')
 
         decoded_object_instance = decode_object_instance(
             remaining_buffer, object_instance_description)
